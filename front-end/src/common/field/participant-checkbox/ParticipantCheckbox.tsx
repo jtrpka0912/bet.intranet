4c47ff1bf29b0fe25e7395bca2eaaa579f5f22ee
jtrpka0912 <jtrpka0912@gmail.com>
2024-11-05 19:42:46 +0000

    Added doc block comment.

--- front-end/src/common/field/participant-checkbox/ParticipantCheckbox.tsx
+++ front-end/src/common/field/participant-checkbox/ParticipantCheckbox.tsx
@@ -1,6 +1,12 @@
 import S from './ParticipantCheckbox.module.css';
 
-const ParticipantCheckbox = () => {
+/**
+ * @function ParticipantCheckbox
+ * @description A stylized checkbox that is big and simply checks if participant won or not
+ * @author J. Trpka
+ * @returns {JSX.Element}
+ */
+const ParticipantCheckbox = (): JSX.Element => {
     return (
         <label className={S.participantCheckbox}>
             <input type="checkbox" />
