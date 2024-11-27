ddf59f5cdc7df11533acac0a3d7bbbc9b4b498eb
jtrpka0912 <jtrpka0912@gmail.com>
2024-11-27 13:39:47 +0000

    This is so stupid with IDX. Containerzation is not working with communications.

--- front-end/src/api/bets.ts
+++ front-end/src/api/bets.ts
@@ -4,8 +4,7 @@ import PaginationResponseDTO from "../dto/pagination-response";
 import ResponseDTO from "../dto/response";
 
 // TODO: Change the URL after project is moved out of IDX
-// const URL = 'https://3000-idx-makeabet-1730228503135.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev';
-const URL = 'http://localhost:3000';
+const URL = 'https://3000-idx-makeabet-1730228503135.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev';
 const STATUS_OK = 200;
 const STATUS_CREATED = 201;
 
@@ -20,7 +19,9 @@ const STATUS_CREATED = 201;
  * @returns {Promise<ResponseDTO<PaginationResponseDTO<BetResponseDTO>>>} 
  */
 export const retrieveBets = async (page: number = 0, limit: number = 20): Promise<ResponseDTO<PaginationResponseDTO<BetResponseDTO>>> => {
-    const response: Response = await fetch(`${URL}/api/v1/bets?page=${page}&limit=${limit}`);
+    const response: Response = await fetch(`${URL}/api/v1/bets?page=${page}&limit=${limit}`, {
+        credentials: 'include'
+    });
 
     const json: ResponseDTO<PaginationResponseDTO<BetResponseDTO>> = await response.json();
 
@@ -30,6 +31,13 @@ export const retrieveBets = async (page: number = 0, limit: number = 20): Promis
 
     return json;
 }
+
+/**
+ * curl -X https://3000-idx-makeabet-1730228503135.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev/api/resource \
+  -H "Origin: https://5173-idx-makeabet-1730228503135.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev" \
+  -H "Access-Control-Request-Method: POST" \
+  -H "Access-Control-Request-Headers: Content-Type"
+ */
 
 /**
  * @function createBet
@@ -42,8 +50,9 @@ export const createBet = async (bet: CreateBetRequestBodyDTO): Promise<ResponseD
     const response: Response = await fetch(`${URL}/api/v1/bets`, {
         method: 'POST',
         body: JSON.stringify(bet),
+        credentials: 'include',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         }
     });
 
