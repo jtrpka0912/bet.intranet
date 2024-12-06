1efbdacafc382685b1464f0a6be88fafc0c6bf75
jtrpka0912 <jtrpka0912@gmail.com>
2024-12-06 13:43:59 +0000

    Revamped the input field component, but removed some features.

--- front-end/src/components/layout/Layout.tsx
+++ front-end/src/components/layout/Layout.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import S from './Layout.module.css';
 import Panel from "../common/panel/Panel";
 import ListPanel from '../panels/list/List';
+import InputField from '../common/field/input-field/InputField';
 
 /**
  * @function Layout
@@ -9,14 +11,26 @@ import ListPanel from '../panels/list/List';
  * @returns {JSX.Element}
  */
 const Layout = () => {
+    const [sample, setSample] = React.useState('');
+
+    console.info(sample);
+
     return (
         <main className={S.layout}>
             <ListPanel />
 
             <Panel>
-                <h1>Detail View</h1>
+                <h1>Sandbox</h1>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <InputField id={'sample'}>
+                    <InputField.Label>Sample</InputField.Label>
+                    <InputField.Input
+                        type="datetime-local"
+                        name="sample"
+                        value={sample}
+                        onChange={(e) => setSample(e.target.value)} 
+                    />
+                </InputField>
             </Panel>
         </main>
     );
