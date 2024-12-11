6826f879aa58e95fa7c6bd99cfeb4ff94aa3942c
J. Trpka <jtrpka0912@gmail.com>
2024-12-10 19:58:02 -0500

    Added the fields and a warning message for expired bets.

--- front-end/src/components/panels/detail/Detail.tsx
+++ front-end/src/components/panels/detail/Detail.tsx
@@ -1,6 +1,24 @@
+import S from './Detail.module.css';
 import { useSelector } from "react-redux";
 import Panel from "../../common/panel/Panel";
 import { RootState } from "../../../store";
+import { DetailFieldProps } from "./Detail.types";
+
+/**
+ * @function DetailField
+ * @description A simple component to show the label and the value for the field
+ * @author J. Trpka
+ * @param props
+ * @returns 
+ */
+const DetailField = ({label, value}: DetailFieldProps) => {
+  return (
+    <dl className={S.detailField}>
+      <dt><h3>{label}</h3></dt>
+      <dd><p>{value}</p></dd>
+    </dl>
+  );
+};
 
 /**
  * @function DetailPanel
@@ -15,7 +33,17 @@ const DetailPanel = () => {
   return (
     <Panel>
       {detail ? (
-        <p>{detail.id}</p>
+        <div>
+          { new Date().getTime() > new Date(detail.endsAt).getTime()  ? (
+            <p className={S.detailWarning}>This bet has ended, please complete to confirm!</p>
+          ) : null}
+          <DetailField label="Stipulation" value={detail.stipulation} />
+          <DetailField label="Jeremy's Answer" value={detail.jeremyAnswer} />
+          <DetailField label="Hidemi's Answer" value={detail.hidemiAnswer} />
+          <DetailField label="Jeremy Bets" value={detail.jeremyBets} />
+          <DetailField label="Hidemi Bets" value={detail.hidemiBets} />
+          <DetailField label="Bet Ends At" value={new Date(detail.endsAt).toLocaleString()} />
+        </div>
       ) : (
         <p>Click a bet to show its detail.</p>
       )}
--- front-end/src/components/panels/detail/Detail.types.ts
+++ front-end/src/components/panels/detail/Detail.types.ts
@@ -3,3 +3,8 @@ import BetResponseDTO from "../../../dto/bet-response"
 export type DetailPanelProps = {
   bet?: BetResponseDTO;
 }
+
+export type DetailFieldProps = {
+  label: string;
+  value: string;
+}
