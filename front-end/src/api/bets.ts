e31bb7c5103e1087001948d8ce612b07a42b78f7
J. Trpka <jtrpka0912@gmail.com>
2024-12-11 09:57:33 -0500

    Complete bet api is working!

--- front-end/src/api/bets.ts
+++ front-end/src/api/bets.ts
@@ -1,4 +1,5 @@
 import BetResponseDTO from "../dto/bet-response";
+import CompleteBetRequestBody from "../dto/complete-bet-request-body";
 import CreateBetRequestBodyDTO from "../dto/create-bet-request-body";
 import PaginationResponseDTO from "../dto/pagination-response";
 import ResponseDTO from "../dto/response";
@@ -34,13 +35,13 @@ export const retrieveBets = async (page: number = 0, limit: number = 20): Promis
  * @function createBet
  * @description Create a bet from the backend
  * @author J. Trpka
- * @param {CreateBetRequestBodyDTO} bet 
+ * @param {CreateBetRequestBodyDTO} body 
  * @returns {Promise<ResponseDTO<BetResponseDTO>>}
  */
-export const createBet = async (bet: CreateBetRequestBodyDTO): Promise<ResponseDTO<BetResponseDTO>> => {
+export const createBet = async (body: CreateBetRequestBodyDTO): Promise<ResponseDTO<BetResponseDTO>> => {
     const response: Response = await fetch(`${URL}/api/v1/bets`, {
         method: 'POST',
-        body: JSON.stringify(bet),
+        body: JSON.stringify(body),
         headers: {
             'Content-Type': 'application/json'
         }
@@ -49,6 +50,24 @@ export const createBet = async (bet: CreateBetRequestBodyDTO): Promise<ResponseD
     const json: ResponseDTO<BetResponseDTO> = await response.json();
 
     if(response.status !== STATUS_CREATED || json.error) {
+        throw new Error(json.message);
+    }
+
+    return json;
+}
+
+export const completeBet = async (id: string, body: CompleteBetRequestBody): Promise<ResponseDTO<BetResponseDTO>> => {
+    const response: Response = await fetch(`${URL}/api/v1/bets/${id}/complete`, {
+        method: 'PATCH',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const json: ResponseDTO<BetResponseDTO> = await response.json();
+
+    if(response.status !== STATUS_OK || json.error) {
         throw new Error(json.message);
     }
 
